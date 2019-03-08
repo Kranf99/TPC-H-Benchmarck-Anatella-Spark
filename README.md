55e44b58ba212f41d7280bc13c33a6b67757ae5e
Frank Vanden Berghen <frank@timi.eu>
2019-03-09 00:48:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TPC-H-Benchmarck-Anatella-Spark-
+
+
---
+++ TPC-H spark/src/main/scala/TpchQuery.scala
@@ -0,0 +1,117 @@
+package main.scala
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+import org.apache.spark.sql._
+import scala.collection.mutable.ListBuffer
+
+/**
+ * Parent class for TPC-H queries.
+ *
+ * Defines schemas for tables and reads pipe ("|") separated text files into these tables.
+ *
+ * Savvas Savvides <savvas@purdue.edu>
+ *
+ */
+abstract class TpchQuery {
+
+  // get the name of the class excluding dollar signs and package
+  private def escapeClassName(className: String): String = {
+    className.split("\\.").last.replaceAll("\\$", "")
+  }
+
+  def getName(): String = escapeClassName(this.getClass.getName)
+
+  /**
+   *  implemented in children classes and hold the actual query
+   */
+  def execute(sc: SparkContext, tpchSchemaProvider: TpchSchemaProvider): DataFrame
+}
+
+object TpchQuery {
+
+  def outputDF(df: DataFrame, outputDir: String, className: String): Unit = {
+
+    if (outputDir == null || outputDir == "")
+      df.collect().foreach(println)
+    else
+      //df.write.mode("overwrite").json(outputDir + "/" + className + ".out") // json to avoid alias
+      df.write.mode("overwrite").format("com.databricks.spark.csv").option("header", "true").save(outputDir + "/" + className)
+  }
+
+  def executeQueries(sc: SparkContext, schemaProvider: TpchSchemaProvider, queryNum: Int): ListBuffer[(String, Float)] = {
+
+    // if set write results to hdfs, if null write to stdout
+    // val OUTPUT_DIR: String = "/tpch"
+    val OUTPUT_DIR: String = "file://" + new File(".").getAbsolutePath() + "/dbgen/output"
+
+    val results = new ListBuffer[(String, Float)]
+
+    var fromNum = 1;
+    var toNum = 22;
+    if (queryNum != 0) {
+      fromNum = queryNum;
+      toNum = queryNum;
+    }
+	
+    for (queryNo <- fromNum to toNum) {
+      val t0 = System.nanoTime()
+
+      val query = Class.forName(f"main.scala.Q${queryNo}%02d").newInstance.asInstanceOf[TpchQuery]
+
+      outputDF(query.execute(sc, schemaProvider), OUTPUT_DIR, query.getName())
+
+      val t1 = System.nanoTime()
+
+      val elapsed = (t1 - t0) / 1000000000.0f // second
+      results += new Tuple2(query.getName(), elapsed)
+
+    }
+	
+    return results
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    var queryNum = 0;
+    if (args.length > 0)
+      queryNum = args(0).toInt
+
+    val conf = new SparkConf().setAppName("Simple Application")
+    val sc = new SparkContext(conf)
+
+    // read files from local FS
+    //val INPUT_DIR_PARQUET = "file://" + new File(".").getAbsolutePath() + "/dbgen"
+    val INPUT_DIR = "file://" + new File(".").getAbsolutePath() + "/dbgen"
+    
+
+    // read from hdfs
+    //val INPUT_DIR: String = "hdfs://192.168.71.128:9000/data"
+
+    val schemaProvider = new TpchSchemaProvider(sc, INPUT_DIR)
+    /*
+    schemaProvider.customer.write.parquet(INPUT_DIR_PARQUET+"/customer.parquet")
+    schemaProvider.lineitem.write.parquet(INPUT_DIR_PARQUET+"/lineitem.parquet")
+    schemaProvider.nation.write.parquet(INPUT_DIR_PARQUET+"/nation.parquet")
+    schemaProvider.region.write.parquet(INPUT_DIR_PARQUET+"/region.parquet")
+    schemaProvider.order.write.parquet(INPUT_DIR_PARQUET+"/orders.parquet")
+    schemaProvider.part.write.parquet(INPUT_DIR_PARQUET+"/part.parquet")
+    schemaProvider.partsupp.write.parquet(INPUT_DIR_PARQUET+"/partsupp.parquet")
+    schemaProvider.supplier.write.parquet(INPUT_DIR_PARQUET+"/supplier.parquet")
+    */
+    val output = new ListBuffer[(String, Float)]
+    output ++= executeQueries(sc, schemaProvider, queryNum)
+
+    val outFile = new File("TIMES.txt")
+    val bw = new BufferedWriter(new FileWriter(outFile, true))
+
+    output.foreach {
+      case (key, value) => bw.write(f"${key}%s\t${value}%1.8f\n")
+    }
+
+    bw.close()
+  }
+}
